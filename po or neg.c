e66a2d1cb24e6d1b5d16516ee63c78b46b0637ff
vinitha98 <31506076+vinitha98@users.noreply.github.com>
2018-01-17 08:32:11 -0800

    Create po or neg.c

---
+++ po or neg.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main() 
+printf("Enter any integer"):
+scanf("%d",&n);
+if(n<=0)
+{
+if(n==0)
+{
+printf("Given no is zero");
+}
+else
+{
+printf("Given no is negative");
+}
+}
+else
+{
+printf("Given no is positive");
+}
+return 0;
+}
