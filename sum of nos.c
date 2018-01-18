f8f5c8bbaaaf33b5f639764035463f8cdf38d6f8
vinitha98 <31506076+vinitha98@users.noreply.github.com>
2018-01-18 06:16:53 -0800

    Create sum of nos.c

---
+++ sum of nos.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int N,i,sum=0;
+printf("Enter the no");
+scanf("%d",&n)
+for(i=1;i<=N;i++)
+{
+sum=sum+i;
+}
+printf("sum of N numbers is %d",sum);
+return 0;
+}
