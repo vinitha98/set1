f0933ddfb7e3ce9d472fec353f74a8a417ac277a
vinitha98 <31506076+vinitha98@users.noreply.github.com>
2018-02-02 16:19:55 +0530

    Create 28array.c

---
+++ 28array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a[10],i,n;
+printf("Enter the number");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+printf("%d\t%d\n",a[i],i);
+}
+return o;
+}
