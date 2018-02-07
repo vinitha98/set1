faccb98db0ca06c43661aa07958b4acfe5408a86
vinitha98 <31506076+vinitha98@users.noreply.github.com>
2018-02-07 16:46:12 +0530

    Create 43concat.c

---
+++ 43concat.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	char a[10],b[5];
+	printf("Enter the string of a");
+	scanf("%s",&a);
+	printf("Enter the string of b");
+	scanf("%s",&b);
+	printf("concatenated string is %s%s",a,b);
+	return 0;
+}
