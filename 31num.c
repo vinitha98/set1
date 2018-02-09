f035c1b8cab773b3080e940701b733f6bc64c379
vinitha98 <31506076+vinitha98@users.noreply.github.com>
2018-02-09 16:04:53 +0530

    Create 31num.c

---
+++ 31num.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int i,count=0;
+	char a[100];
+	scanf("%[^\n]s",&a[i]);
+	for(i=0;i<=NULL;i++)
+	{
+		if(a[i]!=' ')
+		{
+			count++;
+		}
+	}
+	printf("%d",count);
+	return 0;
+}
