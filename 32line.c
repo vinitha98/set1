b996eca79dd421b679389143bb7628c8925a61ab
vinitha98 <31506076+vinitha98@users.noreply.github.com>
2018-02-09 16:18:43 +0530

    Create 32line.c

---
+++ 32line.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int i,count=0;
+	char a[100]="laptop is good";
+	for(i=0;a[i]!=0;i++)
+	{
+		if(a[i]==' ')
+		{
+			count++;
+		}
+	}
+	printf("%d",count+1);
+	return 0;
+}
