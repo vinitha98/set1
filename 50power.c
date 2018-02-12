e56677c5dd1be61099d109efe1fb026f21e10277
vinitha98 <31506076+vinitha98@users.noreply.github.com>
2018-02-12 16:58:05 +0530

    Create 50power.c

---
+++ 50power.c
@@ -0,0 +1,17 @@
+  #include <stdio.h>
+            int main()
+            {
+            	int n;
+            	scanf("%d",&n);
+            	if(n%2==0)
+            	{
+     
+            	printf("yes");
+     
+            	}
+            	else
+            	{
+            		printf("no");
+                }
+            	return 0;
+            }
