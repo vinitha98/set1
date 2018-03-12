1ad806eeccd06df95fb3801e8326df00f6a6ff23
vinitha98 <31506076+vinitha98@users.noreply.github.com>
2018-03-12 16:40:10 +0530

    Create 73num.c

---
+++ 73num.c
@@ -0,0 +1,16 @@
+ #include<stdio.h>
+  int main()
+  {
+  	int N,L,R;
+  	scanf("%d",&N);
+  	scanf("%d%d",&L,&R);
+  	if(N>L&&N<R)
+  	{
+  		printf("yes");
+    	}
+    	else
+    	{
+    		printf("no");
+    	}
+    	return 0;
+  }
