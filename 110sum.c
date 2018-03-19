7149d9a3fa04d667afb1280b367452620b9711bf
vinitha98 <31506076+vinitha98@users.noreply.github.com>
2018-03-19 16:39:20 +0530

    Create 110sum.c

---
+++ 110sum.c
@@ -0,0 +1,19 @@
+ #include<stdio.h>
+ int main()
+ {
+ 	int i,n,a[100],max;
+ 	for(i=0;i<10;i++)
+ 	{
+ 		scanf("%d",&a[i]);
+ 		max=a[0];
+ 	}
+ 	for(i=0;i<10;i++)
+ 	{
+ 		if(a[i]>max)
+ 		{
+ 			max=a[i];
+ 		}
+ 	}
+ 		printf("%d",max);
+ 		return 0;
+ 	}
